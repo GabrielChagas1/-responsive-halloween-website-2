1fc9d881ac056ff1864d820b948cdf1a9ed4361e
Gabriel Chagas <gabrielserq@outlook.com>
2022-10-23 14:55:33 -0300

    chore: creating scroll reveal animate.

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,3 +73,11 @@ const sr = ScrollReveal({
     delay: 400,
     //reset: true //Animations repeat
 });
+
+sr.reveal(`.home__data, .footer__content, .footer__logo, .footer__description`)
+sr.reveal(`.home__tree-1`, {origin: 'left', delay: 800})
+sr.reveal(`.home__tree-2`, {origin: 'right', delay: 800})
+sr.reveal(`.home__img`, {delay: 800})
+sr.reveal(`.category__card, .items__card`, {interval: 100})
+sr.reveal(`.about__img, .about__data, .footer__tree-2`, {origin: 'left'})
+sr.reveal(`.party__images, .party__data, .footer__tree-1`, {origin: 'rigth'})
